a7daf80619f9387925b46bfbfaa4c820b8b2e639
Paulo Magalhães <pauloaxn@gmail.com>
2023-11-17 04:00:55 -0300

    init

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['three'],
+  /**
+   * Enable static exports for the App Router.
+   * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+   */
   output: 'export',
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+  basePath: '/magalhaespaulo.github.io',
+  */
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+  images: {
+    unoptimized: true,
+  },
+  */
+
+  transpilePackages: ['three'],
   experimental: {
     scrollRestoration: false,
   },
